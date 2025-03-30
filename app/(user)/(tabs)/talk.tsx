85ca9bd1f80b198644f784d488f5ce227c95c9be
taro-dragon <60747010+taro-dragon@users.noreply.github.com>
2025-03-30 16:02:36 +0900

    Crate talk empty

--- app/(user)/(tabs)/talk.tsx
+++ app/(user)/(tabs)/talk.tsx
@@ -1,15 +1,40 @@
 import TalkItem from "@/components/talks/TalkItem";
+import { useTheme } from "@/contexts/ThemeContext";
 import { useStore } from "@/hooks/useStore";
-import { FlatList, View } from "react-native";
+import { MessageSquare } from "lucide-react-native";
+import { FlatList, Text, View } from "react-native";
 
 const Talk = () => {
   const { talks } = useStore();
+  const { colors, typography } = useTheme();
   return (
     <View style={{ flex: 1 }}>
       <FlatList
         data={talks}
         renderItem={({ item }) => <TalkItem talk={item} />}
         keyExtractor={(item) => item.id}
+        contentContainerStyle={{ padding: 16, flex: 1 }}
+        ListEmptyComponent={
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 16,
+            }}
+          >
+            <MessageSquare
+              size={48}
+              color={colors.iconSecondary}
+              strokeWidth={1.5}
+            />
+            <Text
+              style={{ color: colors.textSecondary, ...typography.heading2 }}
+            >
+              トークがありません
+            </Text>
+          </View>
+        }
       />
     </View>
   );
