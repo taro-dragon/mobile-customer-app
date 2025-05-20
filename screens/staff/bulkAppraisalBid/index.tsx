f12975a60471c924f68692a14a6b31e7112c10c9
taro-dragon <60747010+taro-dragon@users.noreply.github.com>
2025-05-20 13:35:10 +0900

    create empty

--- screens/staff/bulkAppraisalBid/index.tsx
+++ screens/staff/bulkAppraisalBid/index.tsx
@@ -1,11 +1,16 @@
-import { View } from "react-native";
+import { Dimensions, Text, View } from "react-native";
 import { FlashList } from "@shopify/flash-list";
 
 import BulkApprisalBidItem from "@/components/staff/bulkAppraisalBid/BulkApprisalBidItem";
 import { useBulkAppraisalContext } from "@/contexts/staff/BulkAppraisalContext";
+import { useTheme } from "@/contexts/ThemeContext";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { Inbox } from "lucide-react-native";
 
 const BulkAppraisalBidScreen = () => {
   const { requests } = useBulkAppraisalContext();
+  const { colors, typography } = useTheme();
+  const headerHeight = useHeaderHeight();
   return (
     <FlashList
       data={requests}
@@ -15,6 +20,27 @@ const BulkAppraisalBidScreen = () => {
       ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
       keyExtractor={(item, index) => `${item.id}-${index}`}
       renderItem={({ item }) => <BulkApprisalBidItem item={item} />}
+      ListEmptyComponent={() => (
+        <View
+          style={{
+            height: Dimensions.get("window").height - headerHeight,
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 16,
+            }}
+          >
+            <Inbox size={48} color={colors.iconSecondary} strokeWidth={1.5} />
+            <Text style={{ color: colors.textSecondary, ...typography.body2 }}>
+              対象の査定依頼がありません
+            </Text>
+          </View>
+        </View>
+      )}
     />
   );
 };
