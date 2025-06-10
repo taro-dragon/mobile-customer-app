ffac7a0a7cd9bdd9d938d2470a5ef7373842432c
taro-dragon <60747010+taro-dragon@users.noreply.github.com>
2025-06-11 02:20:14 +0900

    Update

--- components/StockCar/StoreTab.tsx
+++ components/StockCar/StoreTab.tsx
@@ -3,16 +3,34 @@ import { Text, View } from "react-native";
 import { Tabs } from "react-native-collapsible-tab-view";
 
 import { Shop } from "@/types/models/Shop";
+import { useTheme } from "@/contexts/ThemeContext";
+import CarInfoItem from "../CarDetail/CarInfoIten";
 
 type StoreTabProps = {
   store: Shop;
 };
 
 const StoreTab: React.FC<StoreTabProps> = ({ store }) => {
+  const { colors, typography } = useTheme();
   return (
     <Tabs.ScrollView>
-      <View>
-        <Text>{store.shopName}</Text>
+      <View style={{ padding: 16, gap: 16 }}>
+        <View style={{ gap: 8 }}>
+          <Text style={{ ...typography.heading3, color: colors.textPrimary }}>
+            店舗情報情報
+          </Text>
+          <View
+            style={{
+              gap: 8,
+              borderWidth: 1,
+              borderColor: colors.borderPrimary,
+              padding: 12,
+              borderRadius: 12,
+            }}
+          >
+            <CarInfoItem label="店舗名" value={store.shopName} />
+          </View>
+        </View>
       </View>
     </Tabs.ScrollView>
   );
