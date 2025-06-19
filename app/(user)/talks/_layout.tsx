1cdff3801f66d6f505b9daae48f31684e7397ac3
taro-dragon <60747010+taro-dragon@users.noreply.github.com>
2025-06-20 00:53:28 +0900

    Update

--- app/(user)/talks/_layout.tsx
+++ app/(user)/talks/_layout.tsx
@@ -1,11 +1,8 @@
 import { useTheme } from "@/contexts/ThemeContext";
-import { Stack, useRouter } from "expo-router";
-import { X } from "lucide-react-native";
-import { TouchableOpacity } from "react-native";
+import { Stack } from "expo-router";
 
 const TalksLayout = () => {
   const { colors } = useTheme();
-  const router = useRouter();
   return (
     <Stack
       screenOptions={{
