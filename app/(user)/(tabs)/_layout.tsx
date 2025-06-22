91c0711edeb41c11490e17db2761d73f1ea82b83
taro-dragon <60747010+taro-dragon@users.noreply.github.com>
2025-06-23 05:56:28 +0900

    Update

--- app/(user)/(tabs)/_layout.tsx
+++ app/(user)/(tabs)/_layout.tsx
@@ -1,22 +1,27 @@
 import { useTheme } from "@/contexts/ThemeContext";
+import { useRegistrationGuard } from "@/hooks/useRegistrationGuard";
 import useUserInfoData from "@/hooks/useUserInfoData";
 import useUserNotification from "@/hooks/useUserNotification";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 
-import { Tabs } from "expo-router";
+import { Tabs, useRouter } from "expo-router";
 import {
   House,
   MessageSquare,
+  Plus,
   Settings,
   ShoppingCart,
   Tag,
 } from "lucide-react-native";
 import { FormProvider, useForm } from "react-hook-form";
+import { TouchableOpacity } from "react-native";
 
 export default function TabLayout() {
   useUserInfoData();
   useUserNotification();
   const { colors } = useTheme();
+  const router = useRouter();
+  const guard = useRegistrationGuard();
   const form = useForm();
   return (
     <FormProvider {...form}>
@@ -52,6 +57,16 @@ export default function TabLayout() {
               title: "売る",
               tabBarIcon: ({ color }) => <Tag size={24} color={color} />,
               headerShadowVisible: false,
+              headerRight: () => (
+                <TouchableOpacity
+                  style={{
+                    marginRight: 16,
+                  }}
+                  onPress={guard(() => router.push("/registrationCar"))}
+                >
+                  <Plus size={24} color={colors.primary} />
+                </TouchableOpacity>
+              ),
             }}
           />
           <Tabs.Screen
