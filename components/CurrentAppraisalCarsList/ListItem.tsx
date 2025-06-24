c2dfc14e68ccaa500e47a7d0ca9e171687509e89
taro-dragon <60747010+taro-dragon@users.noreply.github.com>
2025-06-25 01:58:34 +0900

    Update

--- components/CurrentAppraisalCarsList/ListItem.tsx
+++ components/CurrentAppraisalCarsList/ListItem.tsx
@@ -19,6 +19,8 @@ const ListItem: React.FC<ListItemProps> = ({ car }) => {
         borderRadius: 8,
         width: 144,
         overflow: "hidden",
+        borderWidth: 1,
+        borderColor: colors.borderPrimary,
       }}
     >
       <View style={{ width: "100%", aspectRatio: 1 }}>
@@ -27,16 +29,36 @@ const ListItem: React.FC<ListItemProps> = ({ car }) => {
           style={{ width: "100%", height: "100%" }}
         />
       </View>
-      <View style={{ padding: 8 }}>
-        <Text style={{ ...typography.heading3, color: colors.primary }}>
-          {carData.model.name}
-        </Text>
-        <Text style={{ ...typography.body3, color: colors.textSecondary }}>
-          {carData.maker.name}
-        </Text>
+      <View style={{ padding: 8, gap: 8 }}>
+        <View style={{ gap: 2 }}>
+          <Text
+            numberOfLines={1}
+            style={{ ...typography.heading3, color: colors.primary }}
+          >
+            {carData.model.name}
+          </Text>
+          <Text
+            numberOfLines={1}
+            style={{ ...typography.body3, color: colors.textSecondary }}
+          >
+            {carData.maker.name}
+          </Text>
+        </View>
 
         <View>
-          <Text>{car.bids.length}</Text>
+          <Text style={{ ...typography.body3, color: colors.textPrimary }}>
+            入札件数：
+            <Text
+              style={{
+                ...typography.heading1,
+                color:
+                  car.bids.length > 0 ? colors.primary : colors.textSecondary,
+              }}
+            >
+              {car.bids.length}
+            </Text>
+            件
+          </Text>
         </View>
       </View>
     </View>
