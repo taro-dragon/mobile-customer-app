c9ecee6dfbea05c97c66b1c21ee59ea21ced4f61
taro-dragon <60747010+taro-dragon@users.noreply.github.com>
2025-07-15 04:07:43 +0900

    Update

--- components/staff/projects/detail/CarSection.tsx
+++ components/staff/projects/detail/CarSection.tsx
@@ -5,7 +5,9 @@ import { Bid } from "@/types/firestore_schema/bids";
 import { BuyOffer } from "@/types/firestore_schema/buyOffers";
 import { Stock } from "@/types/firestore_schema/stock";
 import { Image } from "expo-image";
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { ChevronRight } from "lucide-react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 // 型ガード関数
 const isStock = (data: any): data is Stock => {
@@ -27,6 +29,7 @@ type CarSectionProps = {
 
 const CarSection: React.FC<CarSectionProps> = ({ project, carData }) => {
   const { colors, typography } = useTheme();
+  const router = useRouter();
   const isCarInquiry = project.type === "car_inquiry";
   const targetCarData = isCarInquiry
     ? project.targetStockCarData
@@ -34,35 +37,41 @@ const CarSection: React.FC<CarSectionProps> = ({ project, carData }) => {
 
   const buyOffer = project.buyOffer;
   const bid = project.bid;
-
-  console.log(JSON.stringify(buyOffer, null, 2));
-  console.log("isBuyOffer", isBuyOffer(project));
-
   return (
     <View style={{ gap: 8 }}>
       <Text style={{ ...typography.title5, color: colors.textPrimary }}>
         車両詳細
       </Text>
-      <View style={{ flexDirection: "row", gap: 8 }}>
+      <TouchableOpacity style={{ flexDirection: "row", gap: 8 }}>
         <Image
           source={{ uri: targetCarData?.images.front }}
           style={{ width: 72, height: 72, borderRadius: 8 }}
         />
-        <View style={{ gap: 4 }}>
-          <Text style={{ ...typography.body2, color: colors.textSecondary }}>
-            {carData.maker.name}
-          </Text>
-          <Text style={{ ...typography.title3, color: colors.textPrimary }}>
-            {carData.model.name}
-          </Text>
-          <Text style={{ ...typography.body3, color: colors.textSecondary }}>
-            {carData.year.year}
-          </Text>
-          <Text style={{ ...typography.body3, color: colors.textSecondary }}>
-            {carData.grade.gradeName}
-          </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flex: 1,
+          }}
+        >
+          <View style={{ gap: 4 }}>
+            <Text style={{ ...typography.body2, color: colors.textSecondary }}>
+              {carData.maker.name}
+            </Text>
+            <Text style={{ ...typography.title3, color: colors.textPrimary }}>
+              {carData.model.name}
+            </Text>
+            <Text style={{ ...typography.body3, color: colors.textSecondary }}>
+              {carData.year.year}
+            </Text>
+            <Text style={{ ...typography.body3, color: colors.textSecondary }}>
+              {carData.grade.gradeName}
+            </Text>
+          </View>
+          <ChevronRight size={24} color={colors.textSecondary} />
         </View>
-      </View>
+      </TouchableOpacity>
       {isStock(targetCarData) && (
         <View style={{ flexDirection: "row", gap: 8 }}>
           <View style={{ flex: 1, gap: 4 }}>
